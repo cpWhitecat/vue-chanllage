d51cbb299935d47531547d24d7b3394cc5314f40
cpWhitecat <19803cp@gmail.com>
2022-07-14 21:18:59 +0800

    算是个进步了，自己做出来的，不过bug是跟答案对比出来来的

--- src/medium/effectScope/index.vue
+++ src/medium/effectScope/index.vue
@@ -1,13 +1,20 @@
 <script setup lang="ts">
-import { ref, computed, watch, watchEffect } from "vue"
+import { ref, computed, watch, watchEffect, effectScope } from "vue"
 
 const counter = ref(1)
 const doubled = computed(() => counter.value * 2)
 
 // 使用 `effectScope` API 使这些Effect效果在触发一次后停止
+const scope = effectScope();
 
-watch(doubled, () => console.log(doubled.value))
-watchEffect(() => console.log("Count: ", doubled.value))
+scope.run(()=>{
+  watch(doubled, () => console.log(doubled.value))
+  watchEffect(() => console.log("Count: ", doubled.value))
+});
+
+scope.stop();
+
+
 
 counter.value = 2
 
---
+++ src/medium/useToggle/index.vue
@@ -0,0 +1,29 @@
+<script setup lang='ts'>
+import { reactive, ref , toRefs } from 'vue';
+
+
+/**
+ * 实现一个切换状态的可组合函数
+ * 确保该功能正常工作
+*/
+function useToggle(flag:boolean) {  //这边的boolean不能加 这会导致return 的值必须包含boolean类型
+    let AA = ref(flag)
+
+
+
+    const toggle = ()=>{
+        AA.value = ! AA.value
+    }
+    return [AA,toggle]
+};
+
+const [state, toggle] = useToggle(false)
+
+</script>
+
+<template>
+  <p>State: {{ state ? 'ON' : 'OFF' }}</p>
+  <p @click="toggle">
+    Toggle state
+  </p>
+</template>
