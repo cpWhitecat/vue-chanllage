c31e79c22c35b8559cb6712ca935fa15aa5f4bdd
cpWhitecat <19803cp@gmail.com>
2022-07-15 22:06:27 +0800

    想法是对的 ， 但实现错了

---
+++ src/medium/v-focus/index.vue
@@ -0,0 +1,32 @@
+<script setup lang='ts'>
+import { ref , resolveDirective, watch ,  } from "vue"
+
+const state = ref(false)
+
+/**
+ * 实现一个自定义指令,让元素获取焦点
+ * 确保当切换`state`时,元素随着状态值获取/失去焦点
+ *
+*/
+
+const VFocus = {
+    update(){
+        if(state){
+            focus()
+        }else{
+            blur()
+        }
+    }
+}
+
+
+
+setInterval(() => {
+  state.value = !state.value
+}, 2000)
+
+</script>
+
+<template>
+  <input v-focus="state" type="text">
+</template>
