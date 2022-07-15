2a0a665d0a7f91d91d6c90717d688aa8b06bf68b
cpWhitecat <19803cp@gmail.com>
2022-07-15 20:44:44 +0800

    localStorage

--- src/medium/localStorage/index.vue
+++ src/medium/localStorage/index.vue
@@ -8,12 +8,15 @@ import { ref , computed } from "vue"
 function useLocalStorage(key: string, initialValue: any) {
   const value = ref(initialValue)
 	const localValue = computed({
-    get:initialValue=>{
-      return localStorage.getItem(key)
+    get:()=>{
+      localStorage.getItem(key);
+      return value.value  //原先这里是返回localStorage.getItem(key) 这会存在一个问题 万一get不到呢 所以下面才会报错
 			
     },
-    set:initialValue=>{
-			localStorage.setItem(key,value.value)
+    set:newVal=>{
+      value.value = newVal //之前的错误找到了 ，set方法忘记给被proxy的值赋值了 ，传值也搞错了
+			return localStorage.setItem(key,newVal)
+      
   	}
   })
   return localValue
@@ -27,5 +30,15 @@ console.log(counter.value)
 // 同样地,我们也可以通过触发`counter`的`setter`来设置本地存储的值
 
 counter.value = 1
+const update = ()=>{
+  counter.value++
+}
 
 </script>
+
+<template>
+  <p>Counter: {{ counter }}</p>
+  <button @click="update">
+    Update
+  </button>
+</template>
